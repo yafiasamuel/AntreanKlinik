bcdba9ce52dd56264ef7849d397bae328a284b7a
dytra <vancielnugraha@gmail.com>
2017-03-26 23:15:26 +0700

    add PasienServlet functionality

--- src/java/controller/PasienServlet.java
+++ src/java/controller/PasienServlet.java
@@ -5,13 +5,17 @@
  */
 package controller;
 
+import dao.PasienDAO;
+import dao.PasienDAOLocal;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Pasien;
+import service.PasienFacade;
 
 /**
  *
@@ -19,31 +23,44 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "PasienServlet", urlPatterns = {"/PasienServlet"})
 public class PasienServlet extends HttpServlet {
-
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-   * methods.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
+  @EJB
+  private PasienDAOLocal pasienDao;
+ 
   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    try (PrintWriter out = response.getWriter()) {
-      /* TODO output your page here. You may use following sample code. */
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet PasienServlet</title>");      
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Servlet PasienServlet at " + request.getContextPath() + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
+    String action = request.getParameter("action");
+    String id_pasienStr = request.getParameter("id_pasien");
+    int id_pasien = id_pasienStr.equals("") ? 0 : Integer.parseInt(id_pasienStr);
+    String nama = request.getParameter("nama");
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    String tempat_lahir = request.getParameter("tempat_lahir");
+    String tanggal_lahir = request.getParameter("tanggal_lahir");
+    String jenis_kelamin = request.getParameter("jenis_kelamin");
+    String status = request.getParameter("status");
+    String pekerjaan = request.getParameter("pekerjaan");
+    String nomor_telepon = request.getParameter("nomor_telepon");
+    String alamat = request.getParameter("alamat");
+    String foto = request.getParameter("foto");
+    
+    Pasien pasien = new Pasien(id_pasien, nama, username, password, tempat_lahir, tanggal_lahir, jenis_kelamin, status, pekerjaan, nomor_telepon, alamat, foto);
+//    PasienFacade pf = new PasienFacade();
+    if(action.equalsIgnoreCase("Add")){
+      pasienDao.addPasien(pasien);
+//      pf.create(pasien);
+      
     }
+    else if(action.equals("Edit")) {
+      pasienDao.editPasien(pasien);
+    }
+    else if(action.equals("Delete")) {
+      pasienDao.deletePasien(id_pasien);
+    } else if(action.equals("Search")) {
+      pasien = pasienDao.getPasien(id_pasien);
+    }
+    request.setAttribute("pasien", pasien);
+    request.setAttribute("allPasien", pasienDao.getAllPasien());
+    request.getRequestDispatcher("pasieninfo.jsp").forward(request, response);
   }
 
   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
